779780c0c5597abb853068a99244d1306514b5b4
henry <hwelles@protonmail.com>
2020-01-30 20:39:51 -0500

    Become even more compliant with the AGPL! Plus, copyright notices are now in javadocs.

--- src/main/java/nyc/welles/minecraftbots/MinecraftBots.kt
+++ src/main/java/nyc/welles/minecraftbots/MinecraftBots.kt
@@ -2,30 +2,60 @@ package nyc.welles.minecraftbots
 
 import nyc.welles.minecraftbots.serverwidemusic.SWMusicCommand
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
+import java.io.File
+import java.nio.file.Files
 
-/*Copyright (C) 2020  Henry0w
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+/**
+ * Copyright (C) 2020  Henry0w
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
 
 class MinecraftBots : JavaPlugin() {
     override fun onEnable() {
-        instance = this
-        server.pluginManager.registerEvents(SWBEvent(), this)
-        getCommand("swmusic")!!.setExecutor(SWMusicCommand())
+        if (!this.dataFolder.exists()) {
+            this.dataFolder.mkdir();
+        }
+        val runnable = object : BukkitRunnable(){
+            override fun run() {
+                println("MinecraftBots: This program/plugin is licensed under the GNU Affero General Public License. You can find the source code at: https://github.com/Henry0w/MinecraftBots")
+                println("MinecraftBots: You can find a copy of the GNU Affero General Public License located in the config folder of this plugin")
+            }
+
+        }
+        runnable.runTaskLater(this, 5L)
+        val asyncMakeConfig = object : BukkitRunnable(){
+            override fun run() {
+                val file = File(dataFolder, "GNU-Affero-General-Public-License.txt")
+                file.parentFile.mkdirs()
+                saveResource("GNU-Affero-General-Public-License.txt", true)
+
+            }
+
+        }
+        asyncMakeConfig.runTaskAsynchronously(this)
+
+        //val link = this.getResource("GNU-Affero-General-Public-License.txt")
+        //Files.copy(link!!, this.)
+        //instance = this
+        //server.pluginManager.registerEvents(SWBEvent(), this)
+        //getCommand("swmusic")!!.setExecutor(SWMusicCommand())
     }
 
     companion object {
+
         fun getInstance(): JavaPlugin? {
             return instance
         }
--- src/main/java/nyc/welles/minecraftbots/youtubebot/ReleventChatEvent.kt
+++ src/main/java/nyc/welles/minecraftbots/youtubebot/ReleventChatEvent.kt
@@ -5,21 +5,21 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
 
-/*Copyright (C) 2020  Henry0w
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
-
+/**
+ * Copyright (C) 2020  Henry0w
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
 class ReleventChatEvent : SWBEvent(), Listener {
     @EventHandler
     fun onPlayerChat(chatEvent: AsyncPlayerChatEvent) {
